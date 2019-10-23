b535ddf6828179b9aecef9bea5f51ef95c946952
KAY <slimshades68@gmail.com>
2019-10-23 16:19:20 +0000

    Auth Layout

--- app/src/main/java/com/engineerskasa/safetapp/Activity/VerifyPhoneActivity.java
+++ app/src/main/java/com/engineerskasa/safetapp/Activity/VerifyPhoneActivity.java
@@ -1,9 +1,11 @@
 package com.engineerskasa.safetapp.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.engineerskasa.safetapp.R;
 import com.engineerskasa.safetapp.Utility.Tools;
@@ -25,5 +27,13 @@ public class VerifyPhoneActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Tools.setSystemBarColor(this, android.R.color.white);
         Tools.setSystemBarLight(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
